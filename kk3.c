55c98b1bad4c0c8f1f77be70d32be5c486f183f4
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-06-05 14:48:10 +0530

    Add files via upload

---
+++ kk3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  char k;
+  printf("enter the character \n");
+  scanf("%c",&k);eee
+  if(k=='A'|| k=='a'|| k=='E'||k=='e'||k=='I'||k=='i'||k=='O'||k=='o'||k=='U'||k=='u')
+  printf("the value is vowel");
+  else
+  printf("the value is consonant
+  return 0;
+  }
